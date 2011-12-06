1c0d1ea22be14aeebdeb2fac5c1c99f71ec7d2f9
jianqing.ebay <jianqing.ebay@gmail.com>
2011-12-05 18:30:14 -0800

    add User model tweak index controller

---
+++ application/models/User.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Short description for class
+ *
+ * Long description for class (if any)...
+ *
+ * @copyright  Copyright (c) 2011 eBay Inc. All Rights Reserved
+ * @license    Confidential - intended only for the use of eBay employees
+ * @version    1.0 (Nov 30, 2011)
+ * @author     jianqsun
+ */
+class Users extends Zend_Db_Table_Abstract {
+   protected $_name = 'users';
+   
+}
