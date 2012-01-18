001758d72bd9055e25e4befdc5e03dfb932c1d28
jianqing.ebay <jianqing.ebay@gmail.com>
2012-01-18 00:51:55 -0800

    test file

---
+++ library/newfile.php
@@ -0,0 +1,33 @@
+<?php
+
+/**
+ * Short description for class
+ *
+ * Long description for class (if any)...
+ *
+ * @copyright  Copyright (c) 2012 eBay Inc. All Rights Reserved
+ * @license    Confidential - intended only for the use of eBay employees
+ * @version    1.0 (Jan 18, 2012)
+ * @author     jianqsun
+ */
+class lower_case {
+    public $var;
+    private $_var;
+
+    /**
+     * Does something interesting
+     *
+     * @param  boolean  $var1  Where something interesting takes place
+     * @param  integer  $var2  How many times something interesting should happen
+     * @throws Exception If something interesting cannot happen
+     * @return Value 
+     */
+    function new_function($var1 = false, $var2 = null) {
+        
+        
+        
+        throw new Exception('Message here.', 22);
+        
+        return $val;
+    }
+}
